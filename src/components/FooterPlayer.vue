e530cb6bc3b5ae16646d478ac1cdbaf0f9800630
Track07.cda <nj_wanghong@hotmail.com>
2018-03-13 23:48:41 +0800

    微小的工作

--- src/components/FooterPlayer.vue
+++ src/components/FooterPlayer.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="footer-player">
-    <b-navbar type="dark" variant="dark" fixed="bottom">
-      <b-navbar-brand href="#">Test</b-navbar-brand>
+    <b-progress :value="Time" :max="TotalTime" height="5px" class="progressbar"></b-progress>
+    <b-navbar fixed="bottom" type="dark" variant="dark" style="padding: 0rem">
+      <b-navbar-brand href="#" style="padding: 0rem">
+        <b-img blank blank-color="#66CCFF" width="75" height="75"></b-img>
+      </b-navbar-brand>
+      <b-nav-item>
+        <div>
+          <b-progress :value="Time" :max="TotalTime" height="5px"></b-progress>    
+        </div>
+      </b-nav-item>
     </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FooterPlayer'
+  name: 'FooterPlayer',
+  data () {
+    return {
+      Time: 75,
+      TotalTime: 100
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .progressbar {
+    color: #000AE9; 
+    background-color: #6A6A6A;
+    position: fixed;
+    right: 0;
+    left: 0;
+    bottom: 75px;
+  }
+</style>
+
