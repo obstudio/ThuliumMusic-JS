f5b8b1ad5ab1c2de2e092d14c7d605b344a58a12
Track07.cda <nj_wanghong@hotmail.com>
2018-03-15 21:19:59 +0800

    修复“The Big Mistake”(忘记歌名和艺术家了）

--- src/components/FooterPlayer.vue
+++ src/components/FooterPlayer.vue
@@ -6,34 +6,42 @@
       </el-aside>
       <el-main style="height: 100px; padding-top: 0;">
         <el-row>
-          <el-col :span="2" style="padding-top: .6rem; color: white">
-            01:15
+          <el-col :span="4" style="color: white; text-align: left; line-height: 1rem; padding-top: .3rem">
+            <h1>Ob Studio</h1>
+            <p>ObFish</p>
           </el-col>
           <el-col :span="20">
-            <el-slider v-model="Time" step="1" :max="TotalTime"></el-slider>
-          </el-col>
-          <el-col :span="2" style="padding-top: .6rem; color: white">
-            01:40
-          </el-col>
-        </el-row>
-        <el-row type="flex" justify="space-around">
-          <el-col :span="4">
-            <el-button type="primary" round="true"><icon name="list" scale="1.2"></icon></el-button>
-          </el-col>
-          <el-col :span="16">
-            <el-button type="primary" round="true"><icon name="repeat" scale="1.2"></icon></el-button>
-            <el-button type="primary" round="true"><icon name="backward" scale="1.2"></icon></el-button>
-            <el-button type="primary" round="true"><icon name="play" scale="1.2"></icon></el-button>
-            <el-button type="primary" round="true"><icon name="forward" scale="1.2"></icon></el-button>
-            <el-button type="primary" round="true"><icon name="random" scale="1.2"></icon></el-button>
-          </el-col>
-          <el-col :span="4">
             <el-row>
-              <el-col :span="4">
-                <icon name="volume-up" scale="1.5" style="color: white; padding-top: 0.5rem;"></icon>
+              <el-col :span="2" style="padding-top: .6rem; color: white">
+                01:15
               </el-col>
               <el-col :span="20">
-                <el-slider v-model="volume"></el-slider>
+                <el-slider v-model="Time" step="1" :max="TotalTime"></el-slider>
+              </el-col>
+              <el-col :span="2" style="padding-top: .6rem; color: white">
+                01:40
+              </el-col>
+            </el-row>
+            <el-row type="flex" justify="space-around">
+              <el-col :span="4">
+                <el-button type="primary" round="true"><icon name="list" scale="1.2"></icon></el-button>
+              </el-col>
+              <el-col :span="16">
+                <el-button type="primary" round="true"><icon name="repeat" scale="1.2"></icon></el-button>
+                <el-button type="primary" round="true"><icon name="backward" scale="1.2"></icon></el-button>
+                <el-button type="primary" round="true"><icon name="play" scale="1.2"></icon></el-button>
+                <el-button type="primary" round="true"><icon name="forward" scale="1.2"></icon></el-button>
+                <el-button type="primary" round="true"><icon name="random" scale="1.2"></icon></el-button>
+              </el-col>
+              <el-col :span="4">
+                <el-row>
+                  <el-col :span="6">
+                    <icon name="volume-up" scale="1.5" style="color: white; padding-top: 0.5rem;"></icon>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-slider v-model="volume"></el-slider>
+                  </el-col>
+                </el-row>
               </el-col>
             </el-row>
           </el-col>
