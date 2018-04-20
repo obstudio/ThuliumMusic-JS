9119364fec6ef0b30d1df6a4e7c706d06bf303c8
jjyyxx <obfishstudio@gmail.com>
2018-04-20 08:25:04 +0800

    marked test

--- src/lib/doc/marked.js
+++ src/lib/doc/marked.js
@@ -7,96 +7,21 @@
 /**
  * Block-Level Grammar
  */
-
 var block = {
   newline: /^\n+/,
   code: /^( {4}[^\n]+\n*)+/,
-  fences: noop,
-  hr: /^ {0,3}((?:- *){3,}|(?:_ *){3,}|(?:\* *){3,})(?:\n+|$)/,
-  heading: /^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/,
-  nptable: noop,
-  blockquote: /^( {0,3}> ?(paragraph|[^\n]*)(?:\n|$))+/,
-  list: /^( *)(bull) [\s\S]+?(?:hr|def|\n{2,}(?! )(?!\1bull )\n*|\s*$)/,
-  html: /^ *(?:comment *(?:\n|\s*$)|closed *(?:\n{2,}|\s*$)|closing *(?:\n{2,}|\s*$))/,
-  def: /^ {0,3}\[(label)\]: *\n? *<?([^\s>]+)>?(?:(?: +\n? *| *\n *)(title))? *(?:\n+|$)/,
-  table: noop,
-  lheading: /^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/,
-  paragraph: /^([^\n]+(?:\n?(?!hr|heading|lheading| {0,3}>|tag)[^\n]+)+)/,
-  text: /^[^\n]+/
-}
-
-block._label = /(?:\\[[\]]|[^[\]])+/
-block._title = /(?:"(?:\\"|[^"]|"[^"\n]*")*"|'\n?(?:[^'\n]+\n?)*'|\([^()]*\))/
-block.def = edit(block.def)
-  .replace('label', block._label)
-  .replace('title', block._title)
-  .getRegex()
-
-block.bullet = /(?:[*+-]|\d+\.)/
-block.item = /^( *)(bull) [^\n]*(?:\n(?!\1bull )[^\n]*)*/
-block.item = edit(block.item, 'gm')
-  .replace(/bull/g, block.bullet)
-  .getRegex()
-
-block.list = edit(block.list)
-  .replace(/bull/g, block.bullet)
-  .replace('hr', '\\n+(?=\\1?(?:(?:- *){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\\n+|$))')
-  .replace('def', '\\n+(?=' + block.def.source + ')')
-  .getRegex()
-
-block._tag = '(?!(?:' +
-      'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code' +
-      '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo' +
-      '|span|br|wbr|ins|del|img)\\b)\\w+(?!:|[^\\w\\s@]*@)\\b'
-
-block.html = edit(block.html)
-  .replace('comment', /<!--[\s\S]*?-->/)
-  .replace('closed', /<(tag)[\s\S]+?<\/\1>/)
-  .replace('closing', /<tag(?:"[^"]*"|'[^']*'|\s[^'"/>\s]*)*?\/?>/)
-  .replace(/tag/g, block._tag)
-  .getRegex()
-
-block.paragraph = edit(block.paragraph)
-  .replace('hr', block.hr)
-  .replace('heading', block.heading)
-  .replace('lheading', block.lheading)
-  .replace('tag', '<' + block._tag)
-  .getRegex()
-
-block.blockquote = edit(block.blockquote)
-  .replace('paragraph', block.paragraph)
-  .getRegex()
-
-/**
- * Normal Block Grammar
- */
-
-block.normal = merge({}, block)
-
-/**
- * GFM Block Grammar
- */
-
-block.gfm = merge({}, block.normal, {
   fences: /^ *(`{3,}|~{3,})[ .]*(\S+)? *\n([\s\S]*?)\n? *\1 *(?:\n+|$)/,
-  paragraph: /^/,
-  heading: /^ *(#{1,6}) +([^\n]+?) *#* *(?:\n+|$)/
-})
-
-block.gfm.paragraph = edit(block.paragraph)
-  .replace('(?!', '(?!' +
-        block.gfm.fences.source.replace('\\1', '\\2') + '|' +
-        block.list.source.replace('\\1', '\\3') + '|')
-  .getRegex()
-
-/**
- * GFM + Tables Block Grammar
- */
-
-block.tables = merge({}, block.gfm, {
-  nptable: /^ *(\S.*\|.*)\n *([-:]+ *\|[-| :]*)\n((?:.*\|.*(?:\n|$))*)\n*/,
-  table: /^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/
-})
+  hr: /^ {0,3}((?:- *){3,}|(?:_ *){3,}|(?:\* *){3,})(?:\n+|$)/,
+  heading: /^ *(#{1,6}) +([^\n]+?) *#* *(?:\n+|$)/,
+  list: /^( *)((?:[*+-]|\d+\.)) [\s\S]+?(?:\n+(?=\1?(?:(?:- *){3,}|(?:_ *){3,}|(?:\* *){3,})(?:\n+|$))|\n+(?= {0,3}\[((?:\\[[\]]|[^[\]])+)\]: *\n? *<?([^\s>]+)>?(?:(?: +\n? *| *\n *)((?:"(?:\\"|[^"]|"[^"\n]*")*"|'\n?(?:[^'\n]+\n?)*'|\([^()]*\))))? *(?:\n+|$))|\n{2,}(?! )(?!\1(?:[*+-]|\d+\.) )\n*|\s*$)/,
+  def: /^ {0,3}\[((?:\\[[\]]|[^[\]])+)\]: *\n? *<?([^\s>]+)>?(?:(?: +\n? *| *\n *)((?:"(?:\\"|[^"]|"[^"\n]*")*"|'\n?(?:[^'\n]+\n?)*'|\([^()]*\))))? *(?:\n+|$)/,
+  table: /^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/,
+  lheading: /^([^\n]+)\n *(=|-){2,} *(?:\n+|$)/,
+  paragraph: /^([^\n]+(?:\n?(?! *(`{3,}|~{3,})[ .]*(\S+)? *\n([\s\S]*?)\n? *\2 *(?:\n+|$)|( *)((?:[*+-]|\d+\.)) [\s\S]+?(?:\n+(?=\3?(?:(?:- *){3,}|(?:_ *){3,}|(?:\* *){3,})(?:\n+|$))|\n+(?= {0,3}\[((?:\\[[\]]|[^[\]])+)\]: *\n? *<?([^\s>]+)>?(?:(?: +\n? *| *\n *)((?:"(?:\\"|[^"]|"[^"\n]*")*"|'\n?(?:[^'\n]+\n?)*'|\([^()]*\))))? *(?:\n+|$))|\n{2,}(?! )(?!\1(?:[*+-]|\d+\.) )\n*|\s*$)| {0,3}((?:- *){3,}|(?:_ *){3,}|(?:\* *){3,})(?:\n+|$)| *(#{1,6}) +([^\n]+?) *#* *(?:\n+|$)|([^\n]+)\n *(=|-){2,} *(?:\n+|$))[^\n]+)+)/,
+  text: /^[^\n]+/,
+  item: /^( *)((?:[*+-]|\d+\.)) [^\n]*(?:\n(?!\1(?:[*+-]|\d+\.) )[^\n]*)*/,
+  bullet: /(?:[*+-]|\d+\.)/
+}
 
 /**
  * Block Lexer
@@ -106,14 +31,7 @@ class Lexer {
     this.tokens = []
     this.tokens.links = {}
     this.options = options || marked.defaults
-    this.rules = block.normal
-    if (this.options.gfm) {
-      if (this.options.tables) {
-        this.rules = block.tables
-      } else {
-        this.rules = block.gfm
-      }
-    }
+    this.rules = block
   }
   /**
    * Preprocessing
@@ -174,53 +92,11 @@ class Lexer {
         })
         continue
       }
-      // table no leading pipe (gfm)
-      if (top && (cap = this.rules.nptable.exec(src))) {
-        src = src.substring(cap[0].length)
-        item = {
-          type: 'table',
-          header: cap[1].replace(/^ *| *\| *$/g, '').split(/ *\| */),
-          align: cap[2].replace(/^ *|\| *$/g, '').split(/ *\| */),
-          cells: cap[3].replace(/\n$/, '').split('\n')
-        }
-        for (i = 0; i < item.align.length; i++) {
-          if (/^ *-+: *$/.test(item.align[i])) {
-            item.align[i] = 'right'
-          } else if (/^ *:-+: *$/.test(item.align[i])) {
-            item.align[i] = 'center'
-          } else if (/^ *:-+ *$/.test(item.align[i])) {
-            item.align[i] = 'left'
-          } else {
-            item.align[i] = null
-          }
-        }
-        for (i = 0; i < item.cells.length; i++) {
-          item.cells[i] = item.cells[i].split(/ *\| */)
-        }
-        this.tokens.push(item)
-        continue
-      }
       // hr
       if (cap = this.rules.hr.exec(src)) {
         src = src.substring(cap[0].length)
         this.tokens.push({
           type: 'hr'
-        })
-        continue
-      }
-      // blockquote
-      if (cap = this.rules.blockquote.exec(src)) {
-        src = src.substring(cap[0].length)
-        this.tokens.push({
-          type: 'blockquote_start'
-        })
-        cap = cap[0].replace(/^ *> ?/gm, '')
-        // Pass `top` to keep the current
-        // "toplevel" state. This is exactly
-        // how markdown.pl works.
-        this.token(cap, top)
-        this.tokens.push({
-          type: 'blockquote_end'
         })
         continue
       }
@@ -288,19 +164,7 @@ class Lexer {
         })
         continue
       }
-      // html
-      if (cap = this.rules.html.exec(src)) {
-        src = src.substring(cap[0].length)
-        this.tokens.push({
-          type: this.options.sanitize
-            ? 'paragraph'
-            : 'html',
-          pre: !this.options.sanitizer &&
-            (cap[1] === 'pre' || cap[1] === 'script' || cap[1] === 'style'),
-          text: cap[0]
-        })
-        continue
-      }
+
       // def
       if (top && (cap = this.rules.def.exec(src))) {
         src = src.substring(cap[0].length)
@@ -383,8 +247,7 @@ class Lexer {
    * Static Lex Method
    */
   static lex(src, options) {
-    var lexer = new Lexer(options)
-    return lexer.lex(src)
+    return new Lexer(options).lex(src)
   }
 }
 
@@ -1064,8 +927,7 @@ function merge(obj, ...r) {
 /**
  * Marked
  */
-
-export default function marked(src, opt, callback) {
+function marked(src, opt, callback) {
   // throw error in case of non string input
   if (typeof src === 'undefined' || src === null) {
     throw new Error('marked(): input parameter is undefined or null')
@@ -1157,6 +1019,8 @@ export default function marked(src, opt, callback) {
     throw e
   }
 }
+
+module.exports = marked
 
 /**
  * Options
